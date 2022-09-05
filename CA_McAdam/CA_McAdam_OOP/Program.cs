3aac3e74fb0258b16af58f006ec94b7434e94360
Fatih Şallı <mimsallifatih@gmail.com>
2022-09-05 17:54:11 +0300

    .

---
+++ CA_McAdam/CA_McAdam_OOP/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CA_McAdam_OOP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
