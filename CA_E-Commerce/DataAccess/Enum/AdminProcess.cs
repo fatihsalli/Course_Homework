7ccf3707ef49470afab936066b44d2d304fc1bd8
Fatih Şallı <mimsallifatih@gmail.com>
2022-09-19 22:31:57 +0300

    .

---
+++ CA_E-Commerce/DataAccess/Enum/AdminProcess.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Enum
+{
+    internal enum AdminProcess
+    {
+        ProductCRUD,
+        CategoryCRUD,
+        OrderReport,
+        TopSellingProduct,
+        TopSellingStore
+    }
+}
---
+++ CA_E-Commerce/DataAccess/Enum/UserType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Enum
+{
+    internal enum UserType
+    {
+        Customer=1,
+        Guest,
+        Admin
+    }
+}
